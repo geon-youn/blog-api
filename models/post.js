72000d3415723e1218c974a54f9004ce63e21718
geon-youn <geon.youn@outlook.com>
2023-09-19 16:53:23 -0400

    make modified not required

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const postSchema = new Schema({
   text: { required: true, type: String },
   author: { required: true, type: Schema.Types.ObjectId, ref: 'User' },
   created: { required: true, type: Date },
-  modified: { requried: true, type: Date },
+  modified: { type: Date },
   published: { required: true, type: Boolean },
 });
 
