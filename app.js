bdc66d2f3483915d379c9f64ab519c4181a14380
geon-youn <geon.youn@outlook.com>
2023-09-19 16:08:43 -0400

    remove passport

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const postRouter = require('./routes/postRouter');
 const commentRouter = require('./routes/commentRouter');
 const blogRouter = require('./routes/blogRouter');
 
-const User = require('./models/user');
-
 require('dotenv').config();
 
 const mongoose = require('mongoose', {
@@ -21,55 +19,8 @@ mongoose.connection.on(
   console.error.bind(console, 'mongo connection error')
 );
 
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username });
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-      done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    secret: process.env.secret,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
