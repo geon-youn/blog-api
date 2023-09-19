4708f739b056d7274a0c25f903f1e1c47194817d
geon-youn <geon.youn@outlook.com>
2023-09-19 16:52:44 -0400

    set up crud urls for post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const postController = require('../controllers/postController');
+
+router.post('/create', postController.createPost);
+router.get('/get/:id', postController.getPost);
+router.put('/update/:id', postController.updatePost);
+router.delete('/delete/:id', postController.deletePost);
 
 module.exports = router;
