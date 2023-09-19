9f12fc615c2be6bf18da8b8021c7d6b3331618e5
geon-youn <geon.youn@outlook.com>
2023-09-19 16:51:48 -0400

    include last modified date

--- models/post.js
+++ models/post.js
@@ -6,6 +6,7 @@ const postSchema = new Schema({
   text: { required: true, type: String },
   author: { required: true, type: Schema.Types.ObjectId, ref: 'User' },
   created: { required: true, type: Date },
+  modified: { requried: true, type: Date },
   published: { required: true, type: Boolean },
 });
 
