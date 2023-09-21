e34def910de75c1a96c02f2187abf4af70646e28
geon-youn <geon.youn@outlook.com>
2023-09-21 16:15:13 -0400

    update schema to allow deleting and replying

--- models/comment.js
+++ models/comment.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  text: { required: true, type: String },
-  author: { required: true, type: Schema.Types.ObjectId, ref: 'User' },
+  text: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
   post: { required: true, type: Schema.Types.ObjectId, ref: 'Post' },
+  replyTo: { type: Schema.Types.ObjectId, ref: 'Comment' },
   created: { required: true, type: Date },
 });
 
