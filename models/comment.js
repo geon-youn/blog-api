b702e11d59f8a924410f8f5c9bd8b64a78639fde
geon-youn <geon.youn@outlook.com>
2023-09-21 16:18:12 -0400

    handle comment deletion differently with boolean

--- models/comment.js
+++ models/comment.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  text: { type: String },
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  text: { required: true, type: String },
+  author: { required: true, type: Schema.Types.ObjectId, ref: 'User' },
   post: { required: true, type: Schema.Types.ObjectId, ref: 'Post' },
   replyTo: { type: Schema.Types.ObjectId, ref: 'Comment' },
   created: { required: true, type: Date },
+  deleted: { required: true, type: Boolean },
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
