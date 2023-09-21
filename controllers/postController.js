964e7594cfdbeb50113a861f757f05953d3f5cb4
geon-youn <geon.youn@outlook.com>
2023-09-21 16:08:20 -0400

    implement crud for post

--- controllers/postController.js
+++ controllers/postController.js
@@ -47,6 +47,104 @@ module.exports.createPost = [
   }),
 ];
 
-module.exports.getPost = asyncHandler();
-module.exports.updatePost = asyncHandler();
-module.exports.deletePost = asyncHandler();
+module.exports.getPost = asyncHandler(async (req, res, next) => {
+  const post = await Post.findById(req.params.id).populate(
+    'author',
+    'username first_name last_name _id'
+  );
+
+  return res.json({
+    post,
+  });
+});
+
+module.exports.updatePost = [
+  getToken,
+  body('title').trim().isLength({ min: 1 }).withMessage('title is required'),
+  body('text').trim().isLength({ min: 1 }).withMessage('text is required'),
+  body('published').isBoolean().withMessage('expecting boolean for published'),
+  asyncHandler(async (req, res, next) => {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(401).json({
+        message: "Couldn't find post by given id",
+      });
+    }
+
+    jwt.verify(req.token, process.env.secret, async (err, authData) => {
+      if (err) {
+        return res.status(403).json({
+          message: "Couldn't verify jwt",
+          errors: [err],
+        });
+      }
+      const user = authData.user;
+
+      // Verify request is coming from author
+      if (user._id !== post.author.toString()) {
+        return res.sendStatus(403);
+      }
+
+      // Check for errors
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.json({
+          message: 'Invalid input',
+          errors: errors.array(),
+        });
+      }
+
+      // Update post
+      const newPost = new Post({
+        ...post,
+        title: req.body.title,
+        text: req.body.text,
+        modified: new Date(),
+        published: req.body.published,
+        _id: req.params.id,
+      });
+
+      await Post.findByIdAndUpdate(req.params.id, newPost, {});
+      return res.json({
+        message: 'Updated post',
+        post: newPost,
+      });
+    });
+  }),
+];
+
+module.exports.deletePost = [
+  getToken,
+  asyncHandler(async (req, res, next) => {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.json({
+        message: "Couldn't find post",
+      });
+    }
+
+    jwt.verify(req.token, process.env.secret, async (err, authData) => {
+      if (err) {
+        return res.json({
+          message: "Couldn't verify jwt",
+          errors: [err],
+        });
+      }
+
+      const user = authData.user;
+
+      if (user._id !== post.author.toString()) {
+        return res.sendStatus(403);
+      }
+
+      await Post.findByIdAndDelete(req.params.id);
+      return res.json({
+        message: 'Successfully removed post',
+        post,
+      });
+    });
+  }),
+];
