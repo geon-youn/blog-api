082ebd486d59720d6623063a069788ad3cdad572
geon-youn <geon.youn@outlook.com>
2023-09-24 15:07:55 -0400

    add comments and also delete comments when deleting post

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,17 +4,22 @@ const jwt = require('jsonwebtoken');
 const { getToken } = require('./verification');
 
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 require('dotenv').config();
 
 module.exports.createPost = [
+  // Get jwt token
   getToken,
+
+  // Validate input
   body('title').trim().isLength({ min: 1 }).withMessage('title is required'),
   body('text').trim().isLength({ min: 1 }).withMessage('text is required'),
   body('published').isBoolean().withMessage('expecting boolean for published'),
-  asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
 
+  asyncHandler(async (req, res, next) => {
+    // Check for input errors
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         message: 'Invalid input',
@@ -22,6 +27,7 @@ module.exports.createPost = [
       });
     }
 
+    // Verify jwt
     jwt.verify(req.token, process.env.secret, async (err, authData) => {
       if (err) {
         return res.status(403).json({
@@ -29,8 +35,10 @@ module.exports.createPost = [
         });
       }
 
+      // Get user from decoded jwt
       const user = authData.user;
 
+      // Create and save post
       const post = new Post({
         title: req.body.title,
         text: req.body.text,
@@ -38,8 +46,8 @@ module.exports.createPost = [
         created: new Date(),
         published: req.body.published,
       });
-
       await post.save();
+
       return res.json({
         message: 'Created post',
       });
@@ -48,20 +56,25 @@ module.exports.createPost = [
 ];
 
 module.exports.getPost = [
+  // Validate postid
   param('postid').isMongoId().withMessage('Invalid post id'),
-  asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
 
+  asyncHandler(async (req, res, next) => {
+    // Check for input errors
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.json({
         errors: errors.array(),
       });
     }
+
+    // Find post with `postid`
     const post = await Post.findById(req.params.postid).populate(
       'author',
       'username first_name last_name _id'
     );
 
+    // Return post
     return res.json({
       post,
     });
@@ -69,6 +82,7 @@ module.exports.getPost = [
 ];
 
 module.exports.getAllPosts = asyncHandler(async (req, res, next) => {
+  // Find and return all posts
   const posts = await Post.find().populate(
     'author',
     'username first_name last_name _id'
@@ -80,20 +94,34 @@ module.exports.getAllPosts = asyncHandler(async (req, res, next) => {
 });
 
 module.exports.updatePost = [
+  // Get jwt token
   getToken,
+
+  // Validate input
   body('title').trim().isLength({ min: 1 }).withMessage('title is required'),
   body('text').trim().isLength({ min: 1 }).withMessage('text is required'),
   body('published').isBoolean().withMessage('expecting boolean for published'),
   param('postid').isMongoId().withMessage('Invalid post id'),
-  asyncHandler(async (req, res, next) => {
-    const post = await Post.findById(req.params.postid);
 
+  asyncHandler(async (req, res, next) => {
+    // Check for input errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        message: 'Invalid input',
+        errors: errors.array(),
+      });
+    }
+
+    // Find post with `postid`
+    const post = await Post.findById(req.params.postid);
     if (!post) {
       return res.status(401).json({
         message: "Couldn't find post by given id",
       });
     }
 
+    // Verify route is called by author of the post
     jwt.verify(req.token, process.env.secret, async (err, authData) => {
       if (err) {
         return res.status(403).json({
@@ -101,21 +129,11 @@ module.exports.updatePost = [
           errors: [err],
         });
       }
-      const user = authData.user;
 
       // Verify request is coming from author
+      const user = authData.user;
       if (user._id !== post.author.toString()) {
         return res.sendStatus(403);
-      }
-
-      // Check for errors
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        return res.json({
-          message: 'Invalid input',
-          errors: errors.array(),
-        });
       }
 
       // Update post
@@ -127,8 +145,8 @@ module.exports.updatePost = [
         published: req.body.published,
         _id: req.params.postid,
       });
-
       await Post.findByIdAndUpdate(req.params.postid, newPost, {});
+
       return res.json({
         message: 'Updated post',
         post: newPost,
@@ -138,16 +156,31 @@ module.exports.updatePost = [
 ];
 
 module.exports.deletePost = [
+  // Get jwt token
   getToken,
-  asyncHandler(async (req, res, next) => {
-    const post = await Post.findById(req.params.postid);
 
+  // Validate input
+  param('postid').isMongoId().withMessage('Invalid post id'),
+
+  asyncHandler(async (req, res, next) => {
+    // Check for input errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({
+        message: 'Invalid input',
+        errors: errors.array(),
+      });
+    }
+
+    // Find post with `postid`
+    const post = await Post.findById(req.params.postid);
     if (!post) {
       return res.json({
         message: "Couldn't find post",
       });
     }
 
+    // Verify route is called by author of the post
     jwt.verify(req.token, process.env.secret, async (err, authData) => {
       if (err) {
         return res.json({
@@ -156,15 +189,20 @@ module.exports.deletePost = [
         });
       }
 
+      // Verify user is author of post
       const user = authData.user;
-
       if (user._id !== post.author.toString()) {
         return res.sendStatus(403);
       }
 
+      // Delete post
       await Post.findByIdAndDelete(req.params.postid);
+
+      // Delete comments of post
+      await Comment.deleteMany({ post: req.params.postid });
+
       return res.json({
-        message: 'Successfully removed post',
+        message: 'Successfully removed post and related comments',
         post,
       });
     });
