6947ba7d9051722e47430896dc4a408ffc7138f1
geon-youn <geon.youn@outlook.com>
2023-09-24 14:39:27 -0400

    remove blog route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express();
 const userRouter = require('./routes/userRouter');
 const postRouter = require('./routes/postRouter');
 const commentRouter = require('./routes/commentRouter');
-const blogRouter = require('./routes/blogRouter');
 
 require('dotenv').config();
 
@@ -25,7 +24,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
 app.use('/api/comment', commentRouter);
-app.use('/api/blog', blogRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
